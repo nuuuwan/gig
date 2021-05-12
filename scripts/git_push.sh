60e24a3ad928f3a9a65575884082f9f181fd6a0c
NUWAN SENARATNA <nuwan.senaratna@NUWANS-MacBookPro-2018.local>
2021-05-12 17:39:54 +0530

    Updated .readthedocs.yaml

--- scripts/git_push.sh
+++ scripts/git_push.sh
@@ -1,11 +1,10 @@
 DIST_NAME=gig
 
-python3 setup.py test
-pylint src/$DIST_NAME
+# python3 setup.py test
+# pylint src/$DIST_NAME
 
 git add *
 git commit -m "$1"
-git pull
-git push
+git push origin main
 
 open https://github.com/nuuuwan/$DIST_NAME
