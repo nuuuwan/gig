b48b173eb866cc25a0c96f394f130aba0575c8d8
NUWAN SENARATNA <nuwan.senaratna@NUWANS-MacBookPro-2018.local>
2021-06-01 15:17:17 +0530

    Added install_requires

--- setup.py
+++ setup.py
@@ -28,6 +28,11 @@ setuptools.setup(
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.6",
+
+    install_requires=[
+        'utils-nuuuwan',
+    ],
+
     test_suite='nose.collector',
     tests_require=['nose'],
 )
