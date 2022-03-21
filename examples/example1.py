96e3072c1084473ab64013ec13898afc66bb3869
nuuuwan <nuuuwan@gmail.com>
2022-03-21 10:40:06 +0530

    Added example1

---
+++ examples/example1.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    from gig import ents
+    from gig.ent_types import ENTITY_TYPE
+    provinces = ents.get_entities(ENTITY_TYPE.PROVINCE)
+    print(provinces[0])
+
+    district_index = ents.get_entity_index(ENTITY_TYPE.DISTRICT)
+    print(district_index['LK-11'])
+
+    print(ents.get_entity('LK-1103'))
