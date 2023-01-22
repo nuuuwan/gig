925ccf87508666894c8cc4e981117f271a27b46c
nuuuwan <nuuuwan@gmail.com>
2023-01-22 12:49:47 +0530

    config: python-version: '3.8'

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
     ],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.6",
+    python_requires=">=3.8",
     install_requires=[
         "utils-nuuuwan",
         "fuzzywuzzy",
