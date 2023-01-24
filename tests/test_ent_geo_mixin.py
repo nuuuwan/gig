d1347885fac53abb5e673d5610da240b8696baa7
nuuuwan <nuuuwan@gmail.com>
2023-01-24 09:46:57 +0530

    feat: EntGeoMixin: Added geo

--- tests/test_ent_geo_mixin.py
+++ tests/test_ent_geo_mixin.py
@@ -6,10 +6,37 @@ import matplotlib.pyplot as plt
 
 from gig import Ent
 
+TEST_ENT = Ent.from_id('LK-11')
+
 
 class TestEntGeoMixin(TestCase):
+    def test_url_remote_geo_data_path(self):
+
+        print(TEST_ENT.url_remote_geo_data_path)
+
+        self.assertEqual(
+            TEST_ENT.url_remote_geo_data_path,
+            os.path.join(
+                'https://raw.githubusercontent.com',
+                'nuuuwan',
+                'gig-data',
+                'master',
+                'geo',
+                'district',
+                'LK-11.json',
+            ),
+        )
+
+    def test_raw_geo(self):
+        raw_geo = TEST_ENT.raw_geo()
+        self.assertEqual(len(raw_geo), 2)
+        self.assertEqual(len(raw_geo[0]), 23)
+        self.assertEqual(len(raw_geo[0][0]), 2)
+        self.assertAlmostEqual(raw_geo[0][0][0], 79.84781552236845)
+        self.assertAlmostEqual(raw_geo[0][0][1], 6.956820193543937)
+
     def test_geo(self):
-        for id in ['LK-11']:
+        for id in ['LK-1', 'LK-11', 'LK-1127', 'LK-1127025']:
             ent = Ent.from_id(id)
             geo = ent.geo()
             geo.plot()
@@ -21,3 +48,6 @@ class TestEntGeoMixin(TestCase):
             self.assertTrue(
                 filecmp.cmp(test_png_file_path, control_png_file_path)
             )
+
+    # def test_example_1_density_map(self):
+
