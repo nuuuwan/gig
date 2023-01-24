cc61e82f4ac536921f68f8eb4119cd92f68771a3
nuuuwan <nuuuwan@gmail.com>
2023-01-24 08:45:16 +0530

    feat: EntGeoMixin #STUB

--- src/gig/core/Ent.py
+++ src/gig/core/Ent.py
@@ -1,8 +1,9 @@
 from gig.core.EntBase import EntBase
+from gig.core.EntGeoMixin import EntGeoMixin
 from gig.core.EntGIGMixin import EntGIGMixin
 from gig.core.EntJSONMixin import EntJSONMixin
 from gig.core.EntLoadMixin import EntLoadMixin
 
 
-class Ent(EntBase, EntJSONMixin, EntLoadMixin, EntGIGMixin):
+class Ent(EntBase, EntJSONMixin, EntLoadMixin, EntGIGMixin, EntGeoMixin):
     pass
