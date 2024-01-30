182458a6f7b0d95958070990001d7ea1b3885b5e
nuuuwan <nuuuwan@gmail.com>
2024-01-30 11:08:54 +0530

    various

--- examples/example-2-ethnicity.py
+++ examples/example-2-ethnicity.py
@@ -2,6 +2,7 @@ if __name__ == '__main__':
     from gig import Ent, GIGTable
 
     gig_table = GIGTable('population-ethnicity', 'regions', '2012')
-    ent_colombo = Ent.from_id('LK-11')
+    ent_colombo = Ent.from_id('LK-1130')
     print(ent_colombo)
     print(ent_colombo.gig(gig_table))
+    print(ent_colombo.gig(gig_table).total)
